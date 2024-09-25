4f5b72ba5630982432e8721ae454ea50c030aaa2
Deivy <dpaul_20@hotmail.com>
2024-09-24 22:09:48 -0300

    Refactor redis host configuration to use environment variable

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,7 +2,7 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST ?? "127.0.0.1",
+  host: process.env.REDIS_URL,
   port: parseInt(process.env.REDIS_PORT ?? "6379", 10),
   maxRetriesPerRequest: 5,
   retryStrategy: (times) => {

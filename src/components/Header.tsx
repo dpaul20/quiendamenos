f995d517ed0345399e776db2b6ff1c88852bea44
Deivy <dpaul_20@hotmail.com>
2024-09-27 18:39:28 -0300

    Refactor components and add Cafecito button to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
-import { Alert, AlertDescription } from "./ui/alert";
-import { MoveRight } from "lucide-react";
+import Cafecito from "./Cafecito";
 
 export default function Header() {
   return (
@@ -13,29 +11,7 @@ export default function Header() {
         </h1>
       </div>
 
-      <div className="flex flex-col lg:flex-row justify-between items-center gap-2">
-        <Alert className="flex flex-row items-center gap-2 px-2 py-1 lg:px-4 lg:py-3">
-          <AlertDescription className="text-xs text-center">
-            ¿Conseguiste el mejor precio?
-          </AlertDescription>
-          <div className="hidden lg:block h-4 w-4">
-            <MoveRight className="h-full w-full top-1/2 right-2" />
-          </div>
-        </Alert>
-        <Link
-          href="https://cafecito.app/quien-da-menos"
-          rel="noopener"
-          target="_blank"
-        >
-          <Image
-            src="https://cdn.cafecito.app/imgs/buttons/button_4.png"
-            width={192}
-            height={40}
-            alt="Invitame un café en cafecito.app"
-            className="h-9 lg:h-auto max-h-10 max-w-48"
-          />
-        </Link>
-      </div>
+      <Cafecito />
     </div>
   );
 }
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -15,16 +15,16 @@ export default function SearchForm({ onSearch }: Readonly<SearchFormProps>) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-row gap-2">
+    <form onSubmit={handleSubmit} className="flex flex-row gap-2 w-full max-w-3xl mx-auto">
       <Input
         type="text"
         value={productName}
         onChange={(e) => setProductName(e.target.value)}
         placeholder="Ingrese el nombre del producto"
         required
-        className="w-full border-green-300 focus:ring-green-500 focus:border-green-500"
+        className="w-full border-green-300 focus:ring-green-500 focus:border-green-500 rounded-full"
       />
-      <Button type="submit" className="w-1/5 text-white">
+      <Button type="submit" className="w-1/5 text-white rounded-full">
         Buscar
       </Button>
     </form>
